156435e740920d95c37487bcdc3cafbd252216d6
= <=>
2023-11-10 22:59:21 +0530

    Add Dockerfile

--- api/serializers.py
+++ api/serializers.py
@@ -1,11 +1,25 @@
 from rest_framework import serializers
 
+# local imports
+from ice_cream_truck.models import Flavor
+
+
+class FlavorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Flavor
+        fields = ['name']
+
 
 class FoodPurchaseSerializer(serializers.Serializer):
     customer_id = serializers.IntegerField()
     food_type = serializers.CharField()
     name = serializers.CharField()
     quantity = serializers.IntegerField()
+    flavors = FlavorSerializer(many=True, read_only=True)
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        return data
 
 
 class InventorySerializer(serializers.Serializer):
@@ -13,3 +27,5 @@ class InventorySerializer(serializers.Serializer):
     name = serializers.CharField()
     price = serializers.DecimalField(max_digits=5, decimal_places=2)
     quantity = serializers.IntegerField()
+
+
--- api/views.py
+++ api/views.py
@@ -1,12 +1,11 @@
 from django.db.models import Sum
-from django.shortcuts import render
 
-# Create your views here.
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from ice_cream_truck.models import BaseIceCreamTruckItemFields
+# local imports
+from ice_cream_truck.models import BaseIceCreamTruckItemFields, FlavorType
 from ice_cream_truck.serializers import BaseIceCreamTruckItemFieldsSerializer
 from .serializers import FoodPurchaseSerializer
 from customers.models import Customer
@@ -21,20 +20,24 @@ class BuyFoodView(APIView):
         "customer_id": 1,
         "food_type": "ice_cream",
         "quantity": 2
+        "flavors": "Chocolate,
+        "name":"Vanilla Ice Cream
     }
     """
-    def post(self, request, format=None):
+    def post(self, request):
         serializer = FoodPurchaseSerializer(data=request.data)
         try:
+            flavors = request.data.get('flavor','')
             if serializer.is_valid():
-                customer_id = serializer.validated_data['customer_id']
-                food_type = serializer.validated_data['food_type']
-                name = serializer.validated_data['name']
-                quantity = serializer.validated_data['quantity']
+                serialized_data = serializer.data
+                customer_id = serialized_data['customer_id']
+                food_type = serialized_data['food_type']
+                name = serialized_data['name']
+                quantity = serialized_data['quantity']
 
                 customer = Customer.objects.get(id=customer_id)
 
-                food_item = BaseIceCreamTruckItemFields.objects.filter(food_type=food_type, name=name)
+                food_item = BaseIceCreamTruckItemFields.objects.filter(food_type=food_type, name=name, flavors__name=flavors)
 
                 if food_item.count() > 0 and food_item[0].quantity >= quantity:
                     food_item = food_item[0]
@@ -55,29 +58,32 @@ class BuyFoodView(APIView):
         except BaseIceCreamTruckItemFields.DoesNotExist:
             return Response({"error": "Food item not found"}, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
-            return Response({"error": "Food item not found"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"error": "Internal Server Error"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 class GetInventoryView(APIView):
     """
     API endpoint to get the current inventory of the ice cream truck.
     """
-    def get(self, request, format=None):
+    def get(self, request):
         try:
             queryset = BaseIceCreamTruckItemFields.objects.all().order_by('food_type')
             serializer = BaseIceCreamTruckItemFieldsSerializer(instance=queryset, many=True)
             output_data = serializer.data
+
             total_revenue = Customer.objects.aggregate(total_revenue=Sum('total_spent'))['total_revenue'] or 0
+
             output_data_dict = {}
             for data in output_data:
-                print(data)
                 if data.get('food_type') in output_data_dict:
                     output_data_dict[data.get('food_type')].append(data)
                 else:
                     output_data_dict[data.get('food_type')] = [data]
+            flavors = [flavor[0] for flavor in FlavorType.FLAVOR_CHOICES]
             inventory_data = {
                 "stock": output_data_dict,
-                "total_revenue": total_revenue
+                "total_revenue": total_revenue,
+                "flavors": flavors
             }
             return Response(inventory_data, status=status.HTTP_200_OK)
         except BaseIceCreamTruckItemFields.DoesNotExist:
